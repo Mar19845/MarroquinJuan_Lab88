e9adb00558c2408793e090452f5cf7cbbea5093c
Mar19845 <54874288+Mar19845@users.noreply.github.com>
2020-05-20 21:48:47 -0600

    ...

---
+++ MarroquinJuan_Lab8/Assets/Script/ActivarLuz.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivarLuz : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Light luz;
+    void OnTriggerEnter(Collider other) 
+    {
+        luz.enabled = true;
+    }
+    
+    void OnTriggerExit(Collider other)
+    {
+        luz.enabled = false;
+    }
+}
