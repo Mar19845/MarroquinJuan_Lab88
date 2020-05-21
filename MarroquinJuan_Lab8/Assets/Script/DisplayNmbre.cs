272a8dd15873df351c33a5014b7ecfddb0a0b191
Mar19845 <54874288+Mar19845@users.noreply.github.com>
2020-05-20 22:11:18 -0600

    a

---
+++ MarroquinJuan_Lab8/Assets/Script/DisplayNmbre.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayNmbre : MonoBehaviour
+{
+      // Start is called before the first frame update
+    public Text txt;
+    public string myString;
+    public float fadeTime;
+    public bool displayInfo;
+    void Start()
+    {
+
+        txt = GameObject.FindGameObjectWithTag("Un objeto de por ahi").GetComponent<Text>();
+        txt.color = Color.clear;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        FadeText();
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Screen.lockCursor = false;
+
+    }
+
+
+
+    public void OnMouseOver()
+    {
+        displayInfo = true;
+    }
+
+    public void OnMouseExit()
+    {
+        displayInfo = false;
+    }
+
+    void FadeText() 
+    {
+
+        if (displayInfo)
+        {
+
+            txt.text = myString;
+            txt.color = Color.Lerp(txt.color, Color.black, fadeTime * Time.deltaTime);
+        
+        }
+        else 
+        {
+
+            txt.color = Color.Lerp(txt.color, Color.clear, fadeTime * Time.deltaTime); 
+
+        }
+
+    }
+}
